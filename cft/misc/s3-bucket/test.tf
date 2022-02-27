d36543b0a63432c48e486829ad0f821bac694ab2
Accurics <admin@accurics.com>
2022-02-27 20:45:42 +0530

    Auto Remediation by Accurics, file: terraform/aws/modules/network/main.tf

--- cft/misc/s3-bucket/test.tf
+++ cft/misc/s3-bucket/test.tf
@@ -1,12 +1,16 @@
 resource "aws_s3_bucket" "mybucket" {
   bucket = "mybucket"
-  acl = "public"
+  acl    = "public"
 
   server_side_encryption_configuration {
     rule {
       apply_server_side_encryption_by_default {
       }
     }
+  }
+
+  versioning {
+    enabled = true
   }
 }
 
@@ -41,4 +45,29 @@ resource "aws_s3_bucket_policy" "b" {
       },
     ]
   })
+}
+
+resource "aws_s3_bucket_policy" "mybucketPolicy" {
+  bucket = "${aws_s3_bucket.mybucket.id}"
+
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "mybucket-restrict-access-to-users-or-roles",
+      "Effect": "Allow",
+      "Principal": [
+        {
+          "AWS": [
+            <aws_policy_role_arn>
+          ]
+        }
+      ],
+      "Action": "s3:GetObject",
+      "Resource": "arn:aws:s3:::${aws_s3_bucket.mybucket.id}/*"
+    }
+  ]
+}
+POLICY
 }
